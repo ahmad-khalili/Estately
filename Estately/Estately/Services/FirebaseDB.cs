484920591aa483502c6b5ce118d614c18546706f
Ahmad Khalili <63163965+ahmad-khalili@users.noreply.github.com>
2022-03-19 17:19:58 +0200

    Add Listing GET method to FirebaseDB Services

--- Estately/Estately/Services/FirebaseDB.cs
+++ Estately/Estately/Services/FirebaseDB.cs
@@ -17,5 +17,14 @@ namespace Estately.Services
         {
             client = new FirebaseClient("https://estately-9a428-default-rtdb.europe-west1.firebasedatabase.app/");
         }
+        
+        public ObservableCollection<Listing> getListing()
+        {
+            var listingData = client
+                .Child("Listings")
+                .AsObservable<Listing>()
+                .AsObservableCollection();
+            return listingData;
+        }
     }
 }
