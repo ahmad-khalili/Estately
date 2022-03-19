f860a000fc745bde73489ca188bff688736d17b8
AmeerQatmosh <68341128+AmeerQatmosh@users.noreply.github.com>
2022-03-19 20:41:15 +0200

    Add navigation

---
+++ Estately/Estately/Views/ListingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Estately.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListingPage : ContentPage
+    {
+        public ListingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
