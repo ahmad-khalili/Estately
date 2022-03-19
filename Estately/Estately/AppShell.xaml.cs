97f7603e791cb80b142ea99249c1fa4898654ca9
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-03-19 16:24:09 +0200

    Add project files.

---
+++ Estately/Estately/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Estately.ViewModels;
+using Estately.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Estately
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
