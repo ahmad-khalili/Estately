6d3e01b3fb1c9dba0e539158a45963aad6ab51a7
Abdallah-Nazzal <57283926+Abdallah-Nazzal@users.noreply.github.com>
2022-03-26 21:09:08 +0300

    Add Featured & Location properties

--- Estately/Estately/Models/Listing.cs
+++ Estately/Estately/Models/Listing.cs
@@ -9,8 +9,11 @@ namespace Estately.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
+        public List<string> Features { get; set; }
         public double Price { get; set; }
         public double Size { get; set; }
+        public string Location { get; set; }
+        public string Featured { get; set; }
         public string Location { get; set; }
     }
 }
