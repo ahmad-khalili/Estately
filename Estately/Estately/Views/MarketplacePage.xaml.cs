6861c4c1396e785e843078ff10b4252a314710ca
Abdallah-Nazzal <57283926+Abdallah-Nazzal@users.noreply.github.com>
2022-03-26 21:14:36 +0300

    Add Marketplace Page Logic

--- Estately/Estately/Views/MarketplacePage.xaml.cs
+++ Estately/Estately/Views/MarketplacePage.xaml.cs
@@ -1,19 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Estately.ViewModels;
+using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Estately.Views
 {
     public partial class MarketplacePage : ContentPage
     {
+        string type { get; set; }
+        
+
+        MarketplaceViewModel marketplaceViewModel = new MarketplaceViewModel();
+
         public MarketplacePage()
         {
             InitializeComponent();
         }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var featuredListings = await marketplaceViewModel.FeaturedListings(type);
+            var nearbyListings = await marketplaceViewModel.NearbyListings(type);
+
+            featuredList.ItemsSource = featuredListings;
+            nearbyList.ItemsSource = nearbyListings;
+            type = null;
+           
+        }
+
+        public void AllButtonClicked(object sender, EventArgs eventArgs)
+        {
+            OnAppearing();
+        }
+
+        public void ForSaleButtonClicked(object sender, EventArgs eventArgs)
+        {
+            type = "Sale";
+            OnAppearing();
+        }
+
+        public void ForRentButtonClicked(object sender, EventArgs eventArgs)
+        {
+            type = "Rent";
+            OnAppearing();
+        }
+
     }
 }
