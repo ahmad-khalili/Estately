f187506d2a0b177eb11fb68ec41fcd9c4cb4c99d
shahd-noor <68778771+shahd-noor@users.noreply.github.com>
2022-03-29 00:50:11 +0300

    Update new listing page

--- Estately/Estately/Views/NewListingPage.xaml.cs
+++ Estately/Estately/Views/NewListingPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +14,44 @@ namespace Estately.Views
         public NewListingPage()
         {
             InitializeComponent();
+
+            var picker = new Picker { Title = "Select a Listing Price Currency", TitleColor = Color.Red };
+            picker.Items.Add("USD");
+            picker.Items.Add("NIS");
+            picker.Items.Add("EUR");
+
+            var picker = new Picker { Title = "Select a Property Size Metric", TitleColor = Color.Red };
+            picker.Items.Add("sq.ft");
+            picker.Items.Add("meters squared");
+
+            var picker = new Picker { Title = "Select a Property Type", TitleColor = Color.Red };
+            picker.Items.Add("Residential");
+            picker.Items.Add("Commercial");
+            picker.Items.Add("Industrial");
+            picker.Items.Add("Raw Land");
+            picker.Items.Add("Special Use");
+           
         }
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex = 0)
+            {
+                PriceCurrencyNameLabel.Text = picker.Items[selectedIndex];
+            }
+            else if (selectedIndex = 1)
+            {
+                PropertySizeMetricNameLabel.Text = picker.Items[selectedIndex];
+            }
+            else if (selectedIndex = 2)
+            {
+                PropertyTypeNameLabel.Text = picker.Items[selectedIndex];
+            }
+         
+        }
+
+
     }
 }
