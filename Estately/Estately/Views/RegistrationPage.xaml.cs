71cea479a0e22f8ebf56e31330646a255e3ffef5
shahd-noor <68778771+shahd-noor@users.noreply.github.com>
2022-04-02 15:22:22 +0300

    Add registration page

---
+++ Estately/Estately/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Estately.Views
+{
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+            BindingContext = new RegistrationViewModel();
+        }
+       
+    }
+}
