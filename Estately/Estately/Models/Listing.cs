f9ef9c3a2bd6cbd0cf9664367c0fe0c67d64ea6c
Abdallah-Nazzal <57283926+Abdallah-Nazzal@users.noreply.github.com>
2022-04-16 20:50:28 +0300

    Update Listing.cs

--- Estately/Estately/Models/Listing.cs
+++ Estately/Estately/Models/Listing.cs
@@ -9,7 +9,10 @@ namespace Estately.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
-        public List<string> Features { get; set; }
+        public string Feature1 { get; set; }
+        public string Feature2 { get; set; }
+        public string Feature3 { get; set; }
+        public string Feature4 { get; set; }
         public double Price { get; set; }
         public double Size { get; set; }
         public string Location { get; set; }
